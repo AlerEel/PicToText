88513b66cd239ab22c50a51c2b9bb4afe0d592c4
Aler <talanovalexej@mail.ru>
2025-01-26 01:56:18 +0300

    improve the structure

--- PicToTextFastApi/main.py
+++ PicToTextFastApi/main.py
@@ -2,7 +2,7 @@ import pytesseract
 from PIL import Image
 
 # путь к исполняемому файлу.
-pytesseract.pytesseract.tesseract_cmd = r"C:\Users\User\PycharmProjects\python_Tesseract\Tesseract-OCR\tesseract.exe"
+pytesseract.pytesseract.tesseract_cmd = r"Tesseract-OCR/tesseract.exe"
 
 def teseract_recognition(path_img):
     return pytesseract.image_to_string(Image.open(path_img), lang='rus+eng', config=r'--oem 3 --psm 6')
